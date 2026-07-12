654a784420121011c021e9a762c96963dd826304
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:59:23 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,8 +1,18 @@
+data "azurerm_key_vault_secret" "token" {
+  for_each     = { for k, v in var.source_control_tokens : k => v if v.token_key_vault_id != null && v.token_key_vault_secret_name != null }
+  name         = each.value.token_key_vault_secret_name
+  key_vault_id = each.value.token_key_vault_id
+}
+data "azurerm_key_vault_secret" "token_secret" {
+  for_each     = { for k, v in var.source_control_tokens : k => v if v.token_secret_key_vault_id != null && v.token_secret_key_vault_secret_name != null }
+  name         = each.value.token_secret_key_vault_secret_name
+  key_vault_id = each.value.token_secret_key_vault_id
+}
 resource "azurerm_source_control_token" "source_control_tokens" {
   for_each = var.source_control_tokens
 
-  token        = each.value.token
+  token        = each.value.token != null ? each.value.token : try(data.azurerm_key_vault_secret.token[each.key].value, null)
   type         = each.value.type
-  token_secret = each.value.token_secret
+  token_secret = each.value.token_secret != null ? each.value.token_secret : try(data.azurerm_key_vault_secret.token_secret[each.key].value, null)
 }
 
--- variables.tf
+++ variables.tf
@@ -3,15 +3,23 @@ variable "source_control_tokens" {
 Map of source_control_tokens, attributes below
 Required:
     - token
+    - token_key_vault_id (alternative to token - read from Key Vault instead)
+    - token_key_vault_secret_name (alternative to token - read from Key Vault instead)
     - type
 Optional:
     - token_secret
+    - token_secret_key_vault_id (alternative to token_secret - read from Key Vault instead)
+    - token_secret_key_vault_secret_name (alternative to token_secret - read from Key Vault instead)
 EOT
 
   type = map(object({
-    token        = string
-    type         = string
-    token_secret = optional(string)
+    token                              = string
+    token_key_vault_id                 = optional(string)
+    token_key_vault_secret_name        = optional(string)
+    type                               = string
+    token_secret                       = optional(string)
+    token_secret_key_vault_id          = optional(string)
+    token_secret_key_vault_secret_name = optional(string)
   }))
 }
 
