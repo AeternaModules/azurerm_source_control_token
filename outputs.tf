e1fdb39fc2a0016a2b59f8371bb15e2b86416281
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:30 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,15 @@
-output "source_control_tokens" {
-  description = "All source_control_token resources"
-  value       = azurerm_source_control_token.source_control_tokens
-  sensitive   = true
-}
 output "source_control_tokens_token" {
-  description = "List of token values across all source_control_tokens"
-  value       = [for k, v in azurerm_source_control_token.source_control_tokens : v.token]
+  description = "Map of token values across all source_control_tokens, keyed the same as var.source_control_tokens"
+  value       = { for k, v in azurerm_source_control_token.source_control_tokens : k => v.token }
   sensitive   = true
 }
 output "source_control_tokens_token_secret" {
-  description = "List of token_secret values across all source_control_tokens"
-  value       = [for k, v in azurerm_source_control_token.source_control_tokens : v.token_secret]
+  description = "Map of token_secret values across all source_control_tokens, keyed the same as var.source_control_tokens"
+  value       = { for k, v in azurerm_source_control_token.source_control_tokens : k => v.token_secret }
   sensitive   = true
 }
 output "source_control_tokens_type" {
-  description = "List of type values across all source_control_tokens"
-  value       = [for k, v in azurerm_source_control_token.source_control_tokens : v.type]
+  description = "Map of type values across all source_control_tokens, keyed the same as var.source_control_tokens"
+  value       = { for k, v in azurerm_source_control_token.source_control_tokens : k => v.type }
 }
 
